0f76d33698d86162e433ad33e11fd18d23c8d311
orderly-guoqing <guoqing@orderly.network>
2026-07-10 01:19:51 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "false",
   "VITE_ENABLED_MENUS": "Markets,Portfolio,Leaderboard,Trading,Swap",
-  "VITE_CUSTOM_MENUS": "",
+  "VITE_CUSTOM_MENUS": "$CUMLIQUID TOKEN, https://pump.fun/coin/En8EEFebDgT73E5EQ8if8Fjf64dTjnFGscj26K7xpump",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/cumliquidxyz",
